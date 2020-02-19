a13eb2f3d362271869602678aef3d5c57c756e97
David Bindel <bindel@cs.cornell.edu>
2020-02-19 12:49:14 -0500

    Fix the version 0 CGS.

--- lec/code/orth_cgs0.m
+++ lec/code/orth_cgs0.m
@@ -4,7 +4,7 @@ function [Q] = orth_cgs(A)
   Q = zeros(m,n);
   for j = 1:n
     v = A(:,j);                    % Take the jth original basis vector
-    v = v-Q(:,j-1)*(Q(:,j-1)'*v);  % Make it orthogonal to q_i, i = 1:j-1
+    v = v-Q(:,1:j-1)*(Q(:,1:j-1)'*v);  % Make it orthogonal to q_i, i = 1:j-1
     v = v/norm(v);                 % Normalize what remains
     Q(:,j) = v;                    % Append the result to the basis
   end
